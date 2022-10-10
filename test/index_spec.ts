498c764c4d3d90ff36d4223910f3fc2cfa48ee6b
Andrea <andreac@equip.app>
2022-10-10 14:19:55 +0200

    Set up unit tesitng with mocha

---
+++ test/index_spec.ts
@@ -0,0 +1,11 @@
+// Expect style
+import 'mocha';
+import { expect } from 'chai';
+
+describe('index.ts', function() {
+  // test a functionality
+  it('should add numbers', function() {
+    // add an assertion
+    expect(1).to.equal(1);
+  })
+})
